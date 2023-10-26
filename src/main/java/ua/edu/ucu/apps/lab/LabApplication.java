1193b2d20759d6896684cd167f0b68766c86a152
Kostyantin <116595519+kostyaCS@users.noreply.github.com>
2023-10-26 21:16:34 +0300

    Update LabApplication.java

--- src/main/java/ua/edu/ucu/apps/lab/LabApplication.java
+++ src/main/java/ua/edu/ucu/apps/lab/LabApplication.java
@@ -3,13 +3,9 @@ package ua.edu.ucu.apps.lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class LabApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LabApplication.class, args);
-	}
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(LabApplication.class, args);
+    }
 }
