e284237506b7988f0a25741e25ad8076f8013fea
Kostyantin <116595519+kostyaCS@users.noreply.github.com>
2023-10-26 21:18:04 +0300

    Update LabApplicationTests.java

--- src/test/java/ua/edu/ucu/apps/lab/LabApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/lab/LabApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LabApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
