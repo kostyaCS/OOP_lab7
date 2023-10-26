2da6b2200d656884d56a0a3dfa34620042d53f3a
kostyantin <kostyantin1408@gmail.com>
2023-10-26 21:04:18 +0300

    changes

--- src/test/java/ua/edu/ucu/apps/lab/LabApplicationTests.java
+++ src/test/java/ua/edu/ucu/apps/lab/LabApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab71ApplicationTests {
+class LabApplicationTests {
 
 	@Test
 	void contextLoads() {
