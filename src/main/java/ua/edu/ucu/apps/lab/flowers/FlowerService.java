766bc3f8d259187cffab45d8f70a93711f7c71e0
kostyantin <kostyantin1408@gmail.com>
2023-10-30 09:01:16 +0200

    make it better

--- src/main/java/ua/edu/ucu/apps/lab/flowers/FlowerService.java
+++ src/main/java/ua/edu/ucu/apps/lab/flowers/FlowerService.java
@@ -2,12 +2,14 @@ package ua.edu.ucu.apps.lab.flowers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlowerService {
     private FlowerRepository flowerRepository;
 
+    @Autowired
     public FlowerService(FlowerRepository flowerRepository) {
         this.flowerRepository = flowerRepository;
     }
